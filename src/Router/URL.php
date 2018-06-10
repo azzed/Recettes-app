04b51b3e7cf6c4b4382907cb43d635f1059c3781
azzedine <bouzelmad.azzedin@gmail.com>
2018-06-10 12:36:28 +0200

    class de constante

---
+++ src/Router/URL.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: azzedine
+ * Date: 10/06/18
+ * Time: 11:58
+ */
+
+namespace App\Router;
+
+
+class URL
+{
+    const DELETE_USER = '/Recette-app/?deleteProfil';
+}
